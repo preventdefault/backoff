17faaff3ffb39391791337514d13d94487b69f15
Bartlack, Mario <mario.bartlack@spring-media.de>
2021-02-16 22:46:27 +0100

    add simple integration test running in deno environment

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,4 @@
-import { backoff, BackoffStep } from "./backoff.ts";
-import { exponential, linear } from "./strategies/index.ts";
-
-function predicate({ retry, delay }: BackoffStep): Promise<boolean> {
-  console.log(retry, delay);
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(retry > 5), 1_000);
-  });
-}
-
-const { start } = backoff({
-  predicate,
-  maxRetries: 5,
-  strategy: exponential(),
-});
-
-const { start: startLinear } = backoff({
-  predicate,
-  maxRetries: 5,
-  strategy: linear(),
-});
-
-start().then((arg) => console.log(arg));
-startLinear().then((arg) => console.log(arg));
+export * from "./backoff.ts";
+export * from "./strategies/index.ts";
+export * from "./wait.ts";
+export * from "./random-between.ts";
